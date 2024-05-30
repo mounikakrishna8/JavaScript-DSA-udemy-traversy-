5bea9785920325d6ef717db5408dc4938d32f732
Mounika Krishna <mounikaa.krishna@gmail.com>
2024-05-30 15:40:12 -0600

    reverse-sring

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,14 @@
-function reverseString() {}
+// function reverseString(str) {
+//   //we can not reverse a string using reverse() method. But, we can reverse an array using reverse() method
+//   return str.split('').reverse().join('');
+// }
+
+function reverseString(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
 
 module.exports = reverseString;
