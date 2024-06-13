6b4d50ba6fe8a9d5e76ab34633d2247d16bacd6e
Mounika Krishna <mounikaa.krishna@gmail.com>
2024-06-12 23:58:51 -0600

    palindrome using seperate functions

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,30 @@
-function isPalindrome() {}
+// function isPalindrome(str) {
+//   const formattedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+//   const reversedStr = formattedStr.split('').reverse().join('');
+//   return formattedStr === reversedStr;
+// }
 
+
+function isPalindrome(str) {
+  const formattedStr = removeNonAlphanumeric(str.toLowerCase());
+  return formattedStr;
+}
+function removeNonAlphanumeric(str) {
+  let formattedStr = '';
+  for (let i = 0; i, str.length; i++) {
+    const char = str[i];
+    if (isAlphanumeric(char)) {
+      formattedStr += char;
+    }
+  }
+  return formattedStr;
+}
+function isAlphanumeric(char) {
+  const code = char.charCodeAt(0);
+  return (
+    (code >= 48 && code <= 57) || //Numbers 0-9
+    (code >= 97 && code <= 122) //Lowercase a-z
+  )
+}
+function reverseString(str) { }
 module.exports = isPalindrome;
