be6b69840b23f60893a5fb8edf0235067a2739bf
Mounika Krishna <mounikaa.krishna@gmail.com>
2024-06-28 23:59:31 -0600

    DSA Practice probles

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,11 +1,15 @@
+// function removeDuplicates(arr) {
+//   const uniqueArr = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!uniqueArr.includes(arr[i])) {
+//       uniqueArr.push(arr[i])
+//     }
+//   }
+//   return uniqueArr;
+// }
 function removeDuplicates(arr) {
-  const uniqueArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (!uniqueArr.includes(arr[i])) {
-      uniqueArr.push(arr[i])
-    }
-  }
-  return uniqueArr;
-}
+  console.log(arr);
+  return Array.from(new Set(arr));
 
+}
 module.exports = removeDuplicates;
